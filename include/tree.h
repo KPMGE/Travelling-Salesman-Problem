f85ac1a9b0b690ca8679a43d192109a7b4954b8a
Laratb1 <lara.tagarro@gmail.com>
2022-10-01 13:30:16 -0300

    :sparkles: new and free functions

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,16 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include "city.h"
+
 typedef struct tree TreeNode;
+
+TreeNode *treeNode_new(City *origin, City *destination, double distance);
+
+int empty_tree(TreeNode *t);
+
+void *treeNode_free(TreeNode *t);
+
+
 
 #endif // !TREE_H
--- src/tree.c
+++ src/tree.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "../include/tree.h"
 #include "../include/city.h"
 
@@ -6,3 +9,29 @@ struct tree {
   double distance;
   struct node **children;
 };
+
+TreeNode *treeNode_new(City *origin, City *destination, double distance){
+  TreeNode *t = malloc(sizeof(TreeNode));
+  t->origin = origin;
+  t->destination = destination;
+  t->distance = distance;
+
+  return t;
+}
+
+int empty_tree(TreeNode *t){
+  return t == NULL;
+}
+
+void *treeNode_free(TreeNode *t){
+  if(!empty_tree(t)){
+    free(t->origin);
+    free(t->destination);
+    //free(t->children);
+    free(t);
+  }
+}
+
+City *return_city(TreeNode *tree){
+
+}
