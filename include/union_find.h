a802cad25319129f14833e4b08ada38db11e77ed
KPMGE <jesuskevin254@gmail.com>
2022-10-01 12:36:23 -0300

    :sparkles: add general implementation for union-find

--- include/union_find.h
+++ include/union_find.h
@@ -1,6 +1,15 @@
 #ifndef UNION_FIND_H
 #define UNION_FIND_H
 
+#include "tree.h"
+#include <stdbool.h>
+#include <stddef.h>
+
 typedef struct uf_node UfNode;
+
+UfNode *uf_set_new(void *value);
+UfNode *uf_find(UfNode *uf);
+UfNode *uf_union(UfNode *a, UfNode *b);
+bool uf_connected(UfNode *a, UfNode *b);
 
 #endif // !UNION_FIND_H
--- src/union_find.c
+++ src/union_find.c
@@ -1,1 +1,52 @@
 #include "../include/union_find.h"
+#include <stdlib.h>
+
+struct uf_node {
+  struct uf_node *parent;
+  void *value;
+  int depth;
+};
+
+UfNode *uf_init(void *value) {
+  UfNode *singleton = malloc(sizeof(UfNode));
+  singleton->value = value;
+  singleton->depth = 1;
+  singleton->parent = NULL;
+  return singleton;
+}
+
+UfNode *uf_find(UfNode *elem) {
+  if (!elem)
+    return NULL;
+
+  UfNode *original = elem;
+  // find the root
+  while (elem->parent) {
+    elem = elem->parent;
+  }
+
+  if (elem != original->parent && elem != original) {
+    original->parent = elem; // path compression
+  }
+
+  return elem;
+}
+
+UfNode *uf_union(UfNode *a, UfNode *b) {
+  UfNode *root_a = uf_find(a);
+  UfNode *root_b = uf_find(b);
+  UfNode *newroot = root_a;
+  if (root_a != root_b) {
+    if (root_a->depth > root_b->depth) { // merge by rank
+      root_b->parent = root_a;
+      root_a->depth++;
+    } else {
+      root_a->parent = root_b;
+      root_b->depth++;
+      newroot = root_b;
+    }
+  }
+  return newroot;
+}
+
+bool uf_connected(UfNode *a, UfNode *b) { return uf_find(a) == uf_find(b); }
