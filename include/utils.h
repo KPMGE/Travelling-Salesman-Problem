01ea0e82e69233239744bc1fd269636cb28cb5ec
KPMGE <jesuskevin254@gmail.com>
2022-10-02 09:29:58 -0300

    :fire: remove unneeded trim function

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,4 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-char *trim(char *s);
-
 #endif // !UTILS_H
--- src/utils.c
+++ src/utils.c
@@ -1,18 +1,1 @@
-#include <ctype.h>
-#include <string.h>
-
-static char *ltrim(char *s) {
-  while (isspace(*s))
-    s++;
-  return s;
-}
-
-static char *rtrim(char *s) {
-  char *back = s + strlen(s);
-  while (isspace(*--back))
-    ;
-  *(back + 1) = '\0';
-  return s;
-}
-
-char *trim(char *s) { return rtrim(ltrim(s)); }
+#include "../include/utils.h"
