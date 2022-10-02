02f68598a7009670877f8c9c969b5ccf5570cf07
KPMGE <jesuskevin254@gmail.com>
2022-10-02 18:16:50 -0300

    :rocket: add first working version of mst

--- src/main.c
+++ src/main.c
@@ -8,6 +8,18 @@
 
 Edge **kruskal(size_t vertices, Edge **edges, size_t qtd_edges);
 
+void save_mst(FILE *f, Edge **mst, size_t dimension) {
+  fprintf(f, "NAME: berlin52\n");
+  fprintf(f, "TYPE: MST\n");
+  fprintf(f, "DIMENSION: %zu\n", dimension);
+  fprintf(f, "MST_SECTION\n");
+  for (size_t i = 0; i < dimension; i++) {
+    fprintf(f, "%zu %zu\n", city_id(edge_origin(mst[i])),
+            city_id(edge_destination(mst[i])));
+  }
+  fprintf(f, "EOF");
+}
+
 int main(int argc, char *argv[]) {
   const char *file_path = argv[1];
   FILE *f = fopen(file_path, "r");
@@ -18,12 +30,6 @@ int main(int argc, char *argv[]) {
   printf("dimension: %zu\n", dim);
   // get all cities
   City **cities = parse_cities(f);
-  // display cities
-  // for (int i = 0; i < dim; i++) {
-  //   printf("id: %zu, x: %.2lf, y: %.2lf\n", city_id(cities[i]),
-  //          city_x(cities[i]), city_y(cities[i]));
-  // }
-
   // compute the edges between all cities
   Edge **edges = compute_edges(cities, dim);
   // obtain the actual amount of edges
@@ -31,28 +37,15 @@ int main(int argc, char *argv[]) {
   size_t qtd_edges = (dim2 * (dim2 + 1)) / 2;
 
   sort_edges(edges, qtd_edges);
+  Edge **mst = kruskal(dim, edges, qtd_edges);
 
-  // display edges
-  // for (size_t i = 0; i < qtd_edges; i++) {
-  //   size_t origin_id = city_id(edge_origin(edges[i]));
-  //   size_t destination_id = city_id(edge_destination(edges[i]));
-  //   double distance = edge_distance(edges[i]);
-  //   printf("origin: %zu, destination: %zu, distance: %.2lf\n", origin_id,
-  //          destination_id, distance);
-  // }
-
-  Edge **mst = kruskal(dim, edges, dim2);
-
-  printf("MST:\n\n");
-  for (size_t i = 0; i < dim; i++) {
-    size_t city1_id = city_id(edge_origin(mst[i]));
-    size_t city2_id = city_id(edge_destination(mst[i]));
-    double distance = edge_distance(mst[i]);
-    printf("%zu -> %zu, %.2lf\n", city1_id, city2_id, distance);
-  }
+  FILE *mst_file = fopen("test.mst", "w");
+  assert(f != NULL && "Could not create mst file");
+  save_mst(mst_file, mst, dim);
 
   // clean up memorry
   fclose(f);
+  fclose(mst_file);
   cities_free(cities, dim);
   edges_free(edges, qtd_edges);
 }
@@ -63,11 +56,15 @@ Edge **kruskal(size_t vertices, Edge **edges, size_t qtd_edges) {
   size_t e = 0, i = 0;
 
   // evaluate until we got a complete tree or there is no elements left
-  while (e < (vertices - 1) && i < qtd_edges) {
+  while (i < qtd_edges) {
     Edge *next_edge = edges[i++];
 
-    size_t pos1 = uf_find(uf_set, city_id(edge_origin(next_edge)));
-    size_t pos2 = uf_find(uf_set, city_id(edge_destination(next_edge)));
+    // as the ids on the file start with 1, but on Uf we use an array starting
+    // with 0, we must adjust the value before using find/union operations
+    size_t id1_adjusted = city_id(edge_origin(next_edge)) - 1;
+    size_t id2_adjusted = city_id(edge_destination(next_edge)) - 1;
+    size_t pos1 = uf_find(uf_set, id1_adjusted);
+    size_t pos2 = uf_find(uf_set, id2_adjusted);
 
     if (!uf_connected(uf_set, pos1, pos2)) {
       mst[e++] = next_edge;
