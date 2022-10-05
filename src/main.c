6f17f69ee1306b7f86e0e46f67ba6ed49756e6de
Kevin Carvalho de Jesus <46900633+KPMGE@users.noreply.github.com>
2022-10-05 12:55:48 -0300

    :recycle: free mst

--- src/main.c
+++ src/main.c
@@ -20,17 +20,14 @@ int main(int argc, char *argv[]) {
   // compute the edges between all cities
 
   Edge **edges = compute_edges(cities, dim);
-
-  // free cities
-  // cities_free(cities, dim);
-
   // obtain the actual amount of edges
   uint16_t dim2 = dim - 1;
   size_t qtd_edges = (dim2 * (dim2 + 1)) / 2;
 
-  printf("sort edges...");
   sort_edges(edges, qtd_edges);
   Edge **mst = kruskal(dim, edges, qtd_edges);
+  
+  edges_free(edges, qtd_edges);
 
   FILE *mst_file = fopen("test.mst", "w");
   assert(f != NULL && "Could not create mst file");
@@ -38,7 +35,7 @@ int main(int argc, char *argv[]) {
   save_mst(mst_file, mst, problem_name, dim);
 
   // clean up memorry
-  edges_free(edges, qtd_edges);
+  free(mst);
   fclose(f);
   free(problem_name);
   fclose(mst_file);
