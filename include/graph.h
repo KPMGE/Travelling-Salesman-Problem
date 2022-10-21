9f3f95f9cdc5b2d68e16fb1bc4246baa093162ee
KPMGE <jesuskevin254@gmail.com>
2022-10-21 12:28:31 -0300

    :recycle: add some comments on graph.h file

--- include/graph.h
+++ include/graph.h
@@ -5,8 +5,13 @@
 #include <stdio.h>
 
 typedef struct graph Graph;
+
+// returns a new, allocated graph.
 Graph *graph_new(uint16_t vertices);
+// adds a new edge to the graph.
 void graph_edge_new(Graph *graph, uint16_t src, uint16_t dest);
+// makes a dfs throught the graph, saving it to the file
 void dfs(Graph *graph, uint16_t vertex, FILE *tour_file);
+// free allocated graph and its nodes.
 void graph_free(Graph *graph, uint16_t num_vertices);
 #endif // !GRAPH_H
